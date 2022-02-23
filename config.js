c7f2d73adc6bd229a789cb295e4b09b96474424d
hoanglamnehihihi <100187716+hoanglamnehihihi@users.noreply.github.com>
2022-02-23 12:23:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,15 @@
 const CONFIG = {
     titleWeb: "nhin lam cho' =))",
-    introTitle: 'em ghet chi =))))',
-    introDesc: `nhẹ nhàng mình bên nhau
-    nhẹ nhàng cầm tay nhau..`,
-    btnIntro: 'bà lúc nào cũng xàm hơn tui =)))',
-    title: 'đang học à =)))',
-    desc: 'hoc ma van bam tiep???',
+    introTitle: 'tự nhiên đang học lập trình mà nhớ bà quá =))))',
+    btnIntro: 'click dô là bị khùng =)))',
+    title: 'đang học mà vô đây chi z =)))',
     btnYes: 'tai nho em =))))',
     btnNo: 'hong nho em =)))',
-    question: 'co bi khung ko =)))',
+    question: 'em ghet chi lam =)))',
     btnReply: 'gửi cho bạn <3',
-    reply: 'iu thì iu mà không yêu thì yêu <33333333',
-    mess: 'biết mà =))) mình cũng iu b nhiều =)))',
-    messDesc: 'toi toi em hoc xong em mua gi cho an nhaaa',
+    reply: 'chi thi iu em lớmmmmm',
+    mess: 'b mà =))) mình cũng iu b nhiều =)))',
+    messDesc: 'tối tối em hoc xong em mua gì cho an nhaaa',
     btnAccept: 'ò oke <3',
     messLink: 'https://www.facebook.com/congchuabuoito123'
 }
